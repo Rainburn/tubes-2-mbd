17dc6bfc9d6ffd584e05eeb634898d10f37ca2b5
Kevin <kevinastefano@gmail.com>
2020-11-04 21:23:41 +0700

    transaksi udah bisa > 1

--- execute.py
+++ execute.py
@@ -1,3 +1,6 @@
+# import from simple locking
+# import from occ
+# import from mvcc
 
 file = open("data-input.txt", "r")
 arr = file.read().split("; ")
@@ -5,13 +8,21 @@ arr = file.read().split("; ")
 array_transaksi = []
 for data in arr:
     jenis = data[0]
-    transaksi = data[1]
     if jenis != "C":
-        item = data[3]
+        item = data[-2]
+        transaksi = data[1:-3]
     else:
         item = ""
+        transaksi = data[1:]
+
     array_transaksi.append((jenis, transaksi, item))
 
 print(array_transaksi)
-
 file.close()
+print("Pilihan input :\n1. Simple Locking\n2. OCC\n3. MVCC")
+inputUser = str(input("Input : "))
+# if inputUser=="1":
+
+# else if inputUser=="2":
+
+# else :
