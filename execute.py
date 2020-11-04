0d791d550e7af313a8f8d12f1824efd00d4b9a0a
Kevin <kevinastefano@gmail.com>
2020-11-04 21:06:38 +0700

    execute with commit

--- execute.py
+++ execute.py
@@ -6,7 +6,10 @@ array_transaksi = []
 for data in arr:
     jenis = data[0]
     transaksi = data[1]
-    item = data[3]
+    if jenis != "C":
+        item = data[3]
+    else:
+        item = ""
     array_transaksi.append((jenis, transaksi, item))
 
 print(array_transaksi)
